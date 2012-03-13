bd6412cd81beb3234189ffa4f7014daf7fb5e89e
Josh Schramm <josh.schramm@gmail.com>
2012-03-13 19:08:25 -0400

    Copied in todo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ATDDRuby::Application.config.secret_token = '73e8a3849dff6b094311da7b867df21dd87c455ea7dc64f82f900465ec80f374c14b74726aa8effc0556665ca345d11d98b915615fe7444c78e654ebeb55c812'
